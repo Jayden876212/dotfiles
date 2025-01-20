23e622add786f8327699bc5badb12e04416d500c
Jayden876212 <dxmqkgax@duck.com>
2025-01-20 06:38:19 +0000

    Lock screen script

---
+++ .scripts/lock.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+lockscreen=/tmp/lockscreen
+mkdir $lockscreen
+
+create_lockscreen_image() {
+	grim -o "$1" "$lockscreen/$1"
+	magick "$lockscreen/$1" -blur 0x20 "$lockscreen/$1"
+}
+create_lockscreen_image "DP-2" &
+create_lockscreen_image "DP-3" &
+create_lockscreen_image "HDMI-A-1" &
+wait
+swaylock --image DP-2:"$lockscreen/DP-2" --image DP-3:"$lockscreen/DP-3" --image HDMI-A-1:"$lockscreen/HDMI-A-1"
+rm -rf $lockscreen
