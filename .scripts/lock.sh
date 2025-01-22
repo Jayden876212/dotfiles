461980aa0d5c4d510070d15949c152cf371afd7d
Jayden876212 <dxmqkgax@duck.com>
2025-01-22 20:04:36 +0000

    Auto-commit

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -28,5 +28,7 @@ ddcutil setvcp D6 0x01 -b 5 --maxtries 15,15,15 &
 hyprctl dispatch dpms on DP-2 &
 
 sleep 3
+pkill eww
+hyprctl dispatch exec eww daemon
 eww reload
 eww open-many topbar0 topbar1 topbar2
