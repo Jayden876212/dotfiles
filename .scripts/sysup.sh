477d2107d5433b7807417b749ee03da486fbabe8
Jayden876212 <dxmqkgax@duck.com>
2025-06-19 17:14:06 +0100

    Auto-commit

--- .scripts/sysup.sh
+++ .scripts/sysup.sh
@@ -3,5 +3,5 @@
 emerge_log="$(doas tail -n 4 /var/log/emerge.log)"
 message="Finished sysup with doas ${emerge_log}"
 
-doas emerge --ask -uDN @world
+doas emerge --ask -uDN --autounmask=y --autounmask-keep-masks=y @world
 dunstify "$message"
